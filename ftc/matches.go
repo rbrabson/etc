4593fb23597403143a510171324a3144ee2d0360
Roy Brabson <rbrabson@gmail.com>
2024-03-07 14:13:59 -0500

    initial

--- ftc/matches.go
+++ ftc/matches.go
@@ -1,5 +1,12 @@
 package ftc
 
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/rbrabson/ftc/internal/ftchttp"
+)
+
 // Type of FTC match
 type MatchType string
 
@@ -32,4 +39,45 @@ type MatchTeam struct {
 	Station    string `json:"station"`
 	DQ         bool   `json:"dq"`
 	OnField    bool   `json:"onField"`
+}
+
+// GetMatchResults returns the results of a given event.
+func GetMatchResults(season string, eventCode string, tournamentLevel MatchType, teamNumber ...string) ([]Match, error) {
+	sb := strings.Builder{}
+	sb.WriteString(server)
+	sb.WriteString("/")
+	sb.WriteString(season)
+	sb.WriteString("/matches/")
+	sb.WriteString(eventCode)
+	sb.WriteString("?tournamentLevel=")
+	sb.WriteString(string(tournamentLevel))
+	if len(teamNumber) > 0 {
+		sb.WriteString("&teamNumber=")
+		sb.WriteString(teamNumber[0])
+	}
+	url := sb.String()
+
+	body, err := ftchttp.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	var output Matches
+	err = json.Unmarshal(body, &output)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the output
+	return output.Matches, nil
+}
+
+func (m Match) String() string {
+	body, _ := json.Marshal(m)
+	return string(body)
+}
+
+func (m MatchTeam) String() string {
+	body, _ := json.Marshal(m)
+	return string(body)
 }
