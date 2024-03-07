97402a389351dde01f4534d5bc44a6e53053fd8a
Roy Brabson <rbrabson@gmail.com>
2024-03-07 12:54:08 -0500

    initial

--- ftc/advancement.go
+++ ftc/advancement.go
@@ -1,10 +1,25 @@
 package ftc
 
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/rbrabson/ftc/internal/ftchttp"
+)
+
 // Advancements is the list of teams that advanced from or to a given tournament
-type Advancements struct {
+type AdvancementsTo struct {
 	AdvancesTo  string        `json:"advancesTo"`
 	Slots       int           `json:"slots"`
 	Advancement []Advancement `json:"advancement"`
+}
+
+type AdvancementsFrom []struct {
+	AdvancedFrom       string        `json:"advancedFrom"`
+	AdvancedFromRegion any           `json:"advancedFromRegion"`
+	Slots              int           `json:"slots"`
+	Advancement        []Advancement `json:"advancement"`
 }
 
 // Advancement is the advancement information for a given team
@@ -16,5 +31,56 @@ type Advancement struct {
 	Status      string `json:"status"`
 }
 
-// GetAdvancemenTo
-// GetAdvancedFrom
+// GetAdvancementFrom returns the source events from which teams advanced from to reach
+// the specified event.
+func GetAdvancementFrom(season string, eventCode string) (*AdvancementsFrom, error) {
+	url := fmt.Sprintf("%s/%s/advancement/%s", server, season, eventCode)
+
+	body, err := ftchttp.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	var output AdvancementsFrom
+	err = json.Unmarshal(body, &output)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the output
+	return &output, nil
+}
+
+// GetAdvancementTo returns the list of teams advancing from the event and to which event
+// they are advancing.
+func GetAdvancementTo(season string, eventCode string, excludeSkipped ...bool) (*AdvancementsTo, error) {
+	sb := strings.Builder{}
+	sb.WriteString(server)
+	sb.WriteString("/")
+	sb.WriteString(season)
+	sb.WriteString("/advancement/")
+	sb.WriteString(eventCode)
+	if len(excludeSkipped) > 0 {
+		sb.WriteString("?excludedSkipped=")
+		if excludeSkipped[0] {
+			sb.WriteString("true")
+		} else {
+			sb.WriteString("false")
+		}
+	}
+	url := sb.String()
+
+	body, err := ftchttp.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	var output AdvancementsTo
+	err = json.Unmarshal(body, &output)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the output
+	return &output, nil
+}
